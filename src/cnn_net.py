b9d36bed015ecc706917a8c587f7e24180329cff
zemota1 <zemariamota@hotmail.com>
2020-02-27 22:59:06 +0000

    Update cnn_net.py

--- src/cnn_net.py
+++ src/cnn_net.py
@@ -5,12 +5,10 @@ class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5, stride=1, padding=0)
-        self.bn1 = nn.BatchNorm2d(20)
         self.relu1 = nn.ReLU()
         self.max_pool1 = nn.MaxPool2d(kernel_size=2)
         self.conv1_drop = nn.Dropout2d(0.5)
         self.conv2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=5, stride=1, padding=0)
-        self.bn2 = nn.BatchNorm2d(50)
         self.relu2 = nn.ReLU()
         self.max_pool2 = nn.MaxPool2d(kernel_size=2)
         self.conv2_drop = nn.Dropout2d(0.5)
@@ -19,12 +17,10 @@ class CNNModel(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
         x = self.relu1(x)
         x = self.max_pool1(x)
         x = self.conv1_drop(x)
         x = self.conv2(x)
-        x = self.bn2(x)
         x = self.relu2(x)
         x = self.max_pool2(x)
         x = self.conv2_drop(x)
